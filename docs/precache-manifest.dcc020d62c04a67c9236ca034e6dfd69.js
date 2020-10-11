299ff036612923c1eb14a4fc58ee37b5fd4a71dd
Gustavo Bono <ggbono27@gmail.com>
2020-10-11 18:52:38 -0300

    docs agregado

---
+++ docs/precache-manifest.dcc020d62c04a67c9236ca034e6dfd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de19cca7763a9738e80360536dc41f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f421aa118f48cdbe522",
+    "url": "/static/css/main.5f1b591a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f421aa118f48cdbe522",
+    "url": "/static/js/main.31c0473d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
